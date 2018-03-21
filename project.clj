128748bfa2dbe40c10d00a305b3c55d71761a6f3
Max Penet <m@qbits.cc>
2018-03-21 18:49:50 +0100

    * add minimal deps.edn
    * bump deps
    
    * sync project.clj with deps.edn

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/ghadishayban/squee"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :profiles {:dev {:plugins [[codox "0.8.12"]]
                    :codox {:include [squee.jdbc squee.datasources squee.impl.protocols]
                            :src-dir-uri "http://github.com/ghadishayban/squee/blob/master/"
